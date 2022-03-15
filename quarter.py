aa688c110407639ca3bcedf2ffcaae9300e32816
Alvaro <alvgaona@gmail.com>
2022-03-15 17:15:44 -0300

    Add quarter

--- quarter.py
+++ quarter.py
@@ -1,2 +1,23 @@
-# -*- coding: utf-8 -*-
+def ask_for_month():
+    return int(input("Insert month: "))
 
+
+def quarter(month):
+    if month <= 3:
+        return "Q1"
+    elif month <= 6:
+        return "Q2"
+    elif month <= 9:
+        return "Q3"
+    elif month <= 12:
+        return "Q4"
+
+
+month = ask_for_month()
+
+while month < 1 or month > 12:
+    month = ask_for_month()
+
+q = quarter(month)
+
+print(f"Usted esta en el {q}")
