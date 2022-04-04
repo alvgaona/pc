f08b8d2cc0736c70fb0479e519d58e556537ccff
Alvaro <alvgaona@gmail.com>
2022-04-04 13:12:03 -0300

    Add DNA solution

--- 04-loops/dna_strand.py
+++ 04-loops/dna_strand.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """
 DNA molecules are made of two twisting, paired strands. Each strand is made of four chemical units,
 called nucleotide bases. The bases are adenine (A), thymine (T), guanine (G) and cytosine (C).
@@ -25,4 +27,102 @@ def compress(strand: str) -> str:
     Returns:
         str: Compressed DNA sequence.
     """
-    pass
+    result = []
+    counter = 1
+    current = strand[0]
+
+    for i in range(1, len(strand) + 1):
+        if i < len(strand):
+            if strand[i] == current:
+                counter += 1
+                continue
+
+        if counter == 1:
+            result.append(current)
+        else:
+            result.append(str(counter))
+            result.append(current)
+            counter = 1
+
+        if i < len(strand):
+            current = strand[i]
+
+    return ''.join(result)
+
+
+def compress_prima(strand: str) -> str:
+    """
+    This function takes the DNA Strand  with the read of the sequencer and encodes it replacing repeated
+    nucleotides with the number of repetitions followed by the repeated nucleotide. For example, given an input of
+    "TTTAGGTC" the program should output "3TA2GTC".
+
+    Args:
+        strand (str): Full DNA sequence.
+
+    Returns:
+        str: Compressed DNA sequence.
+    """
+
+    result = []
+    counter = 1
+
+    for i in range(0, len(strand)):
+        current = strand[i]
+        if i < len(strand) - 1:
+            j = i + 1
+
+            if current == strand[j]:
+                counter += 1
+                continue
+
+        if counter == 1:
+            result.append(current)
+        else:
+            result.append(str(counter))
+            result.append(current)
+            counter = 1
+
+    return ''.join(result)
+
+
+def compress_generator(strand: str):
+    """
+    This function takes the DNA Strand  with the read of the sequencer and encodes it replacing repeated
+    nucleotides with the number of repetitions followed by the repeated nucleotide. For example, given an input of
+    "TTTAGGTC" the program should output "3TA2GTC".
+
+    Args:
+        strand (str): Full DNA sequence.
+
+    Returns:
+        Generator[str]: Compressed DNA sequence.
+    """
+
+    counter = 1
+
+    for i in range(0, len(strand)):
+        current = strand[i]
+        if i < len(strand) - 1:
+            j = i + 1
+
+            if current == strand[j]:
+                counter += 1
+                continue
+
+        if counter == 1:
+            yield current
+        else:
+            yield str(counter) + current
+            counter = 1
+
+
+def main():
+    strand = 'AAACTTTTTAGCCCCCCCGGGGTTTTTAAA'
+
+    assert compress(strand) == '3AC5TAG7C4G5T3A'
+    assert compress_prima(strand) == '3AC5TAG7C4G5T3A'
+    assert ''.join(compress_generator(strand)) == '3AC5TAG7C4G5T3A'
+
+
+if __name__ == '__main__':
+    exit(main())
